22925e404e9afddd3c9b1f5ffabe35d2e6077b38
acezen <acezengwb@gmail.com>
2016-04-18 09:27:02 +0800

    web configuration

---
+++ www/config_default.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# _*_ coding: utf-8 _*_
+
+'''
+Default configuration
+'''
+
+__author__ = 'acezen(Ace Zeng)'
+
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3360,
+        'user': 'www-data',
+        'password': 'www-data',
+        'database': 'awesome'
+    },
+    'session': {
+        'secret': 'AwEs0mE'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# _*_ coding: utf-8 _*_
+
+'''
+Override configuration
+'''
+
+__author__ = 'acezen(Ace Zeng)'
+
+
+configs = {
+    'db': {
+        'host': '127.0.0.1'
+    }
+}
